c069e22f93cf3d63e271fc92e9b6369222c6919f
Rubel-hossain <rubel2585@gmail.com>
2018-05-15 23:39:43 +0600

    test all node modules

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
+
+// express-handlebars config 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 port = 3000;
 
-app.get('/',(req,res)=>res.send('Working Properly'));
+app.get('/', (req,res)=>{
+    const title = "Welcome";
+    res.render('home',{
+        title: title
+    });
+});
 
-app.listen(port,()=>console.log("working"));
+app.get('/about', (req,res)=>{
+    res.render('about');
+})
+
+app.listen(port, ()=>console.log("working"));
